e041426424a8aec901cdd140ebad78269fe27fee
louis890214 <b10702222@gmail.com>
2020-10-27 16:12:29 +0800

    v0.0.7

--- FrontEnd/web/After-Login/Orders/Orders.js
+++ FrontEnd/web/After-Login/Orders/Orders.js
@@ -1,5 +1,32 @@
-const Logout = document.getElementById('Logout');
-Logout.addEventListener('click', function change(){
-    window.sessionStorage.clear();
-    window.location.replace("../../Shopping-Page/Shopping.html");
+fetch('http://140.118.121.100:5000/Customer/Trade_list',{
+method: 'POST',
+headers: {
+    'Accept': 'application/json, text/plain',
+    'Content-Type': 'application/json'
+},
+body: JSON.stringify({
+    "S_Customer_Username":window.sessionStorage.getItem("Username")
 })
+}).then(response => {
+    return response.json()
+    }
+)
+.then( (money) =>{
+    PriceList(money)
+})
+function PriceList(money){
+    let output = ``;
+    console.log(money)
+    money.forEach(function(money){
+        output+= `
+        <tr>
+            <td>${money.S_Trade_Number}</td>
+            <td>${money.S_Goods_Number} </td>
+            <td>${money.S_Trade_Address}</td>
+            <td>魚貨運送中</td>
+            <td>$${money.I_Goods_Total}</td>
+        </tr>
+        `    
+});
+document.getElementById('output').innerHTML = output;
+}
