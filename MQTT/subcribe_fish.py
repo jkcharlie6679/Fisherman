9ed4b51dbc391cbad43ad947be5bdbb4f6be0065
jkcharlie6679 <b10702124@gapps.ntust.edu.tw>
2020-11-04 21:52:26 +0800

    1104 api tsung-yu

--- MQTT/subcribe_fish.py
+++ MQTT/subcribe_fish.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 import os, configparser, psycopg2, json
-from MQTT_to_DB import fish_sent_to_DB 
+from MQTT_to_DB import fish_sent_to_DB, CHT_Blockchain, Find_CHT_Blockchain
 from Ethereum_Transaction import Transaction
 
 path = os.path.abspath('.')
@@ -10,28 +10,31 @@ config.read(cfgpath)
 
 
 def on_connect(client, userdata, flags, rc):
-  print("Connected with result code ", rc)
-  client.subscribe(config["MQTT"]["topic_f"])
+    print("Connected with result code ", rc)
+    client.subscribe(config["MQTT"]["topic_f"])
 
 def on_message(client, userdata, msg):
-  mqtt_data = json.loads(msg.payload)
-  print(mqtt_data)
+    mqtt_data = json.loads(msg.payload)
+    print(mqtt_data)
 
-  pg = psycopg2.connect(database = config['POSTGRES']['account_db'], user = config['POSTGRES']['user'], password = config['POSTGRES']['password'], host = config['POSTGRES']['host'], port = config['POSTGRES']['port'])
-  pgadmin = pg.cursor()
-  pgadmin.execute('SELECT * FROM Fisherman_Account')
-  data_db = pgadmin.fetchall()
+    pg = psycopg2.connect(database = config['POSTGRES']['account_db'], user = config['POSTGRES']['user'], password = config['POSTGRES']['password'], host = config['POSTGRES']['host'], port = config['POSTGRES']['port'])
+    pgadmin = pg.cursor()
+    pgadmin.execute('SELECT * FROM Fisherman_Account')
+    data_db = pgadmin.fetchall()
 
-  S_Blkchain_ID = "NULL"
+    S_Blkchain_ID = "NULL"
 
-  for raw in data_db:
-      if(mqtt_data["S_Fisherman_Account"] == raw[1]):
-          S_Blkchain_ID = raw[11]
+    for raw in data_db:
+        if(mqtt_data["S_Fisherman_Account"] == raw[1]):
+            S_Blkchain_ID = raw[11]
 
-  hash_code = Transaction(mqtt_data, S_Blkchain_ID)
-  print("done1")
-  fish_sent_to_DB(mqtt_data, hash_code)
-  print("done")
+    hash_code = Transaction(mqtt_data, S_Blkchain_ID)
+    print("done1")
+    fish_sent_to_DB(mqtt_data, hash_code)
+    print("done2")
+    CHT_Blockchain(mqtt_data)
+    # Find_CHT_Blockchain()
+    print("done")
 
 
 
@@ -41,11 +44,4 @@ mqtt_client.connect(config["MQTT"]["host"], int(config["MQTT"]["port"]), int(con
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 mqtt_client.loop_forever()
-
-
-
-  
-
-  
-
 
